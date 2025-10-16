7f3c33c6ed1e0658ae399d681cae1bcea09ffed9
Daniel <007danlin@gmail.com>
2025-10-16 18:54:43 +0300

    Update Green.cs

--- Lab4/Green.cs
+++ Lab4/Green.cs
@@ -6,7 +6,22 @@
         {
 
             // code here
-
+            double sum = 0;
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    sum += array[i];
+                    count++;
+                }
+            }
+            double average = sum / count;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                    array[i] = average;
+            }
             // end
 
         }
@@ -15,7 +30,18 @@
             int sum = 0;
 
             // code here
+            int negativeIndex = 0;
+            for (int i = 0; i < array.Length; i++) //ищем индекс первого отриц. эл-та
+            {
+                if (array[i] < 0)
+                {
+                    negativeIndex = i;
+                    break;
+                }
+            }
 
+            for (int i = 0; i < negativeIndex; i++) //перебираем до этого индекса
+                sum += array[i]*array[i];
             // end
 
             return sum;
@@ -25,7 +51,50 @@
             int[] negatives = null;
 
             // code here
+            int max = array[0];
+            int min = array[0];
+            int indexMax = 0;
+            int indexMin = 0;
+            
+            for (int i = 0; i < array.Length; i++) //нашли максимальный и минимальный элементы, а также их индексы
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    indexMax = i;
+                }
+                
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    indexMin = i;
+                }
+            }
 
+            int startIndex = Math.Min(indexMax, indexMin); //учли ситуацию, когда мин. и макс. индекс идут в разном порядке
+            int endIndex = Math.Max(indexMax, indexMin);
+
+            int countNegatives = 0;
+            
+            for (int i = startIndex + 1; i < endIndex; i++) //считали количество отрицательных элементов
+            {                                               //startIndex + и i < ( а не <=) так как не учитываем границы
+                if (array[i] < 0)
+                    countNegatives++;
+            }
+
+            negatives = new int[countNegatives]; //создали пустой список от количества отрицательных элементов
+
+            int indexNegative = 0; //индекс для нового массива
+            
+            for (int i = startIndex + 1; i < endIndex; i++) //перебираем в старом массиве от начального до конечного индекса
+            {                                               //startIndex + и i < ( а не <=) так как не учитываем границы
+                if (array[i] < 0) 
+                {
+                    negatives[indexNegative] = array[i]; //записываем в новый массив элемент из старого в ячейку с номером indexNegative
+                    indexNegative++; //переходим на следующую ячейку
+                }
+            }
+                
             // end
 
             return negatives;
@@ -34,7 +103,35 @@
         {
 
             // code here
-
+            //ищем максимальный элемент и его индекс
+            int max = array[0];
+            int indexMax = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    indexMax = i;
+                }
+            }
+           
+            //ищем первый отрицательный элемент
+            int firstNegativeIndex = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    firstNegativeIndex = i;
+                    break;
+                }
+            }
+            
+            if (firstNegativeIndex >= 0) //если в массиве есть отрицательные числа, то firstNegativeIndex меняется и цикл работает, иначе ничего не происходит
+            {
+                int temporary = array[indexMax];
+                array[indexMax] = array[firstNegativeIndex];
+                array[firstNegativeIndex] = temporary;
+            }
             // end
 
         }
@@ -43,7 +140,35 @@
             int[] answer = null;
 
             // code here
+            int maxElement = array[0];
+            for (int i = 0; i < array.Length; i++) //ищем максимум
+            {
+                if (array[i] > maxElement)
+                    maxElement = array[i];
+            }
 
+            //ищем количество максимальных элементов
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == maxElement)
+                    count++;
+            }
+            
+            //answer становится длины count
+            answer = new int[count];
+
+            //answerIndex это позиция в массиве answer
+            int answerIndex = 0; 
+            
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] == maxElement) //если элемент из старого списка является максимальным
+                {
+                    answer[answerIndex] = i; //в списке answer мы его кладем под номером answerIndex
+                    answerIndex++;
+                }
+            }
             // end
 
             return answer;
