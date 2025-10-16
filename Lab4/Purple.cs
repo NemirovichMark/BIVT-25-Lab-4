f7fce4aa2b5c0d7e364db10feb5f604bb7226305
SaweenSergey <svs0va@yandex.ru>
2025-10-16 17:04:30 +0300

    Add files via upload

--- Lab4/Purple.cs
+++ Lab4/Purple.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab4
+﻿using System.Security.Cryptography;
+
+namespace Lab4
 {
     public class Purple
     {
@@ -6,7 +8,25 @@
         {
 
             // code here
+            double max = double.MinValue, sum = 0;
+            int max_i = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    max_i = i;
+                }
+                sum += array[i];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i > max_i)
+                {
+                    array[i] = (sum / array.Length);
+                }
 
+            }
             // end
 
         }
@@ -15,7 +35,33 @@
             int[] even = null, odd = null;
 
             // code here
-
+            int amount_even = 0, amount_odd = 0;
+            if (array.Length % 2 == 0)
+            {
+                amount_even = array.Length / 2;
+                amount_odd = array.Length / 2;
+            }
+            else
+            {
+                amount_even = array.Length / 2 + 1;
+                amount_odd = array.Length / 2;
+            }
+            even = new int[amount_even];
+            odd = new int[amount_odd];
+            int ei = 0, oi = 0;
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (i % 2 == 0)
+                {
+                    even[ei] = array[i];
+                    ei++;
+                }
+                else
+                {
+                    odd[oi] = array[i];
+                    oi++;
+                }
+            }
             // end
 
             return (even, odd);
@@ -25,7 +71,12 @@
             int[] answer = null;
 
             // code here
+            int e = 1000;
+            double mid = 0;
+            foreach (int i in array)
+            {
 
+            }
             // end
 
             return answer;
