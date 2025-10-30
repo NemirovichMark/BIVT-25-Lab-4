a6fb97d53129f8c4cf4a8445ba21e7fd307561d3
Arina3004 <arina.maksimova2007@mail.ru>
2025-10-30 19:22:07 +0300

    Add files via upload

--- Lab4/White.cs
+++ Lab4/White.cs
@@ -7,9 +7,13 @@
             double length = 0;
 
             // code here
-
+            double sum = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                sum += vector[i] * vector[i];
+            }
+            length = Math.Sqrt(sum);
             // end
-
             return length;
         }
         public int Task2(int[] array, int P, int Q)
@@ -17,7 +21,15 @@
             int count = 0;
 
             // code here
-
+            int amin = Math.Min(Q, P);
+            int amax = Math.Max(Q, P);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > amin && array[i] < amax)
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -26,7 +38,32 @@
         {
 
             // code here
+            if (array != null && array.Length > 1)
+            {
+                int imax = 0;
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if (array[i] > array[imax])
+                        imax = i;
+                }
 
+                if (imax < array.Length - 1)
+                {
+                    int imax = imax + 1;
+                    for (int i = imax + 2; i < array.Length; i++)
+                    {
+                        if (array[i] < array[imax])
+                            imax = i;
+                    }
+
+                    if (array[imax] != array[imax])
+                    {
+                        int temp = array[imax];
+                        array[imax] = array[imax];
+                        array[imax] = temp;
+                    }
+                }
+            }
             // end
 
         }
@@ -34,16 +71,39 @@
         {
 
             // code here
-
+            if (array != 0 && array.Length > 0)
+            {
+                int imax = 0;
+                for (int i = 2; i < array.Length; i += 2)
+                {
+                if (array[i] > array[imax])
+                {
+                    imax = i;
+                }  
+            
+                    array[imax] = imax;
+                }
+            }
+        }
             // end
 
-        }
         public int Task5(int[] array, int P)
         {
             int index = 0;
 
             // code here
-
+            index = -1;
+            if (array != null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == P)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+            }
             // end
 
             return index;
@@ -52,16 +112,54 @@
         {
 
             // code here
-
-            // end
-
+            if (array != null && array.Length > 1)
+            {
+                 int imax = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > array[imax])
+                {
+                    imax = i;
+                }
+                if (imax > 0)
+                    {
+                    for (int i = 0; i < imax - 1; i += 2)
+                        {
+                            int temp = array[i];
+                            array[i] = array[i +1];
+                            array[i + 1] = temp;
+                        }
+                    }
+            }
+            }
         }
+            // end
         public int[] Task7(int[] array)
         {
             int[] answer = null;
 
             // code here
-
+            if (array != 0)
+            {
+                int count = 0;
+                for (int i = 0; i < array.length; i++)
+                {
+                    if (array[i] >= 0)
+                    {
+                        count++;
+                    }
+                }
+                int[] A = new int[count];
+                int index = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] >= 0)
+                    {
+                        A[index++] = array[i];
+                    }
+                }
+                
+            }
             // end
 
             return answer;
@@ -70,7 +168,18 @@
         {
 
             // code here
+            for (int i = 0; i < array.Length - 1;i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
 
+                }
+
+            }
             // end
 
         }
@@ -78,7 +187,14 @@
         {
 
             // code here
-
+            int n = array.Length;
+            int[] reversed = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                reversed[i] = array[n - 1 - i];
+            }
+            return reversed;
+          
             // end
 
         }
@@ -87,6 +203,28 @@
             int[] C = null;
 
             // code here
+            C = A.Length + B.Length;
+            int i = 0, j = 0, k =0;
+            while (i < A.Length && j < B.Length)
+            {
+                if (A[i] <= B[i])
+               
+                {
+                    C[k++] = A[i++];
+                } 
+                else
+                {
+                    C[k++] = B[j++];
+                }
+            }
+            while (i < A.Length)
+            {
+                C[k++] = A[i++];
+            }
+            while (j < B.Length)
+            {
+                C[k++] = B[j++];
+            }
 
             // end
 
@@ -97,7 +235,19 @@
             double[] array = null;
 
             // code here
-
+            if (a == b && n == 1)
+            {
+                return new double[] {a};
+            }
+            else if (n > 1 && a != b)
+            {
+                array = new double[n];
+                double jamp = (b - a) / (n - 1);
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = a + i * jamp;
+                }
+            }
             // end
 
             return array;
@@ -108,7 +258,30 @@
             double[] restored = null;
 
             // code here
+            if (raw != null && raw.Length >= 3)
+            {
+                restored = new double[raw.Length];
+                Array.Copy(raw, restored, raw.Length);
+                if (restored[0] == -1 && restored[1] != -1 && restored[raw.Length - 1] != -1)
+                {
+                    restored[0] = (restored[1] + restored[raw.Length - 1]) / 2;
+                }
+                if (restored[raw.Length - 1] == -1 && restored[0] != -1 && restored[raw.Length - 2] != -1)
+                {
+                    restored[raw.Length - 1] = (restored[0] + restored[raw.Length - 2]) / 2;
+                }
+                for (int i = 1; i < raw.Length - 1; i++)
+                {
+                    double prev = restored[(i - 1)];
+                    double next = restored[(i + 1)];
+                    if (restored[i] == -1 && prev != -1 && next != -1)
+                    {
 
+                        restored[i] = (prev + next) / 2;
+
+                    }
+                }
+            }
             // end
 
             return restored;
